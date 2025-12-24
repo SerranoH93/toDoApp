e2b9b604afece67224586cb117dd700d161f6684
Alejandro Serrano <alejandro_0793@hotmail.com>
2025-12-23 20:50:17 -0600

    Arreglo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,72 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
+import './App.css'
+
+type Todo = {
+  text: string;
+  completed: boolean
+}
 
 function App() {
-  return <h1>Hola Vite</h1>
+    const [text, setText] = useState('');
+    const [todos, setTodos] = useState<Todo[]>(() => {
+      const stored = localStorage.getItem("todos");
+      return stored ? JSON.parse(stored) : [];
+    });
+
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
+    const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!text.trim()) return;
+
+      setTodos([...todos, {text, completed: false}]);
+      setText('');
+    }
+
+    const toggleTodo = (index: number) => {
+      setTodos(
+        todos.map((todo, i) =>
+        i === index ? {...todo, completed: !todo.completed} : todo 
+      )
+      );      
+    };
+
+    const deleteTodo = (index: number) => {
+      setTodos(todos.filter((_, i) => i !== index));
+    };
+
+  return (
+    <div className="app">
+      <h1>To Do</h1>
+
+      <form onSubmit={addTodo}>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Nueva tarea..."
+        />
+        <button type="submit">Agregar</button>
+      </form>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <span
+              onClick={() => toggleTodo(index)}
+              className={todo.completed ? "completed" : ""}
+            >
+              {todo.text}
+            </span>
+
+            <button onClick={() => deleteTodo(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App
